72f8b25fc1856a4a320b3e5cca4bef1231552052
Cléo "Chii Kozume" Lima <cleolimadev@gmail.com>
2024-06-27 16:22:15 -0400

    feature: jest snapshots and global styles

--- my-app/src/components/Main/test.tsx
+++ my-app/src/components/Main/test.tsx
@@ -3,12 +3,23 @@ import Main from '.'
 
 describe('<Main />', () => {
   it('should render the heading', () => {
-    // renderiza o componente
-    render(<Main />)
+    // renderiza o component
+    const { container } = render(<Main />)
 
     // busca o elemento e verifica a existencia
     expect(
       screen.getByRole('heading', { name: /react avançado/i })
     ).toBeInTheDocument()
+
+    // gerar snapshot
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the colors correctly', () => {
+    // renderiza o component
+    const { container } = render(<Main />)
+
+    // verifica se o background-color está correto
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
